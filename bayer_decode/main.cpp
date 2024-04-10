8c6bc39cf78923fab398b943bab1a9f732bf2c60
Nicola-pc-casa <nicolamorelli30008@gmail.com>
2024-04-10 10:51:11 +0200

    ppm

--- bayer_decode/main.cpp
+++ bayer_decode/main.cpp
@@ -4,9 +4,12 @@
 #include "cstdlib"
 #include <cinttypes>
 #include <array>
-#include "algorithm"
+#include <algorithm>
+#include <chrono>
 
+using namespace std::chrono;
 using namespace std;
+
 #define RED 0
 #define GREEN 1
 #define BLUE 2
@@ -16,18 +19,27 @@ class Mat {
     size_t rows_;
     size_t cols_;
     vector<T> data_;
+    T *zero_;
 public:
     Mat(size_t rows, size_t cols) : rows_(rows), cols_(cols) {
-        cout << "MAT CONSTRUCTOR HAS BEEN CALLED!!!" << endl;
-
         data_ = vector<T>(rows_ * cols_);
+        zero_ = nullptr;
     }
 
-    T &operator()(size_t r, size_t c) {
+    T &operator()(int r, int c) {
+        if (zero_ != nullptr) {
+
+            if (r < 0 or c < 0 or r >= rows_ or c >= cols_) {
+                return *zero_;
+            }
+        }
         return data_[r * cols_ + c];
     }
 
-    const T &operator()(size_t r, size_t c) const {
+    const T &operator()(int r, int c) const {
+        if (r < 0 or c < 0 or r >= rows_ or c >= cols_) {
+            return zero_;
+        }
         return data_[r * cols_ + c];
     }
 
@@ -47,23 +59,12 @@ public:
         return cols_;
     }
 
-    ~Mat() {
-        cout << "MAT DECONSTRUCTOR HAS BEEN CALLED!!!" << endl;
+    void setZero(T *zero) {
+        zero_ = zero;
     }
 
 
 };
-
-uint8_t &protected_data(int r, int c, Mat<array<uint8_t, 3>> &data, size_t index) {
-    if (r < 0 or c < 0 or r >= data.rows() or c >= data.cols()) {
-        uint8_t *zero = new uint8_t;
-        *zero = 0;
-        return *zero;
-    } else {
-        return data(r, c)[index];
-    }
-
-}
 
 struct PGMHelper {
 
@@ -85,7 +86,7 @@ struct PGMHelper {
         is >> height;
         int maxval;
         is >> maxval;
-        cout << is.get();
+        is.get();
         data = Mat<uint16_t>(height, width);
         is.read(reinterpret_cast<char *>(data.dataVector().data()), data.dataVector().size() * sizeof(uint16_t));
         std::for_each(data.dataVector().begin(), data.dataVector().end(), [](uint16_t &x) {
@@ -94,8 +95,8 @@ struct PGMHelper {
         return data;
     }
 
-    static auto dumpPGM(Mat<uint8_t> &data, const string &path) {
-        ofstream os(path + "_gray.pgm", std::ios::binary);
+    static auto dumpPGM(Mat<uint8_t> &data, const string &path, const string &prefix) {
+        ofstream os(path + prefix + ".pgm", std::ios::binary);
         if (os.fail()) {
             perror("Error while opening ouput file");
             return EXIT_FAILURE;
@@ -109,8 +110,8 @@ struct PGMHelper {
         return EXIT_SUCCESS;
     }
 
-    static auto dumpPPM(Mat<array<uint8_t, 3>> &data, const string &path) {
-        ofstream os(path + ".ppm", std::ios::binary);
+    static auto dumpPPM(Mat<array<uint8_t, 3>> &data, const string &path, const string &prefix) {
+        ofstream os(path + prefix + ".ppm", std::ios::binary);
         if (os.fail()) {
             perror("Error while opening ouput file");
             return EXIT_FAILURE;
@@ -128,98 +129,83 @@ struct PGMHelper {
 Mat<uint8_t> &quantize(Mat<uint16_t> &data16, Mat<uint8_t> &data8) {
     for (int r = 0; r < data8.rows(); ++r) {
         for (int c = 0; c < data8.cols(); ++c) {
-            data8(r, c) = static_cast<uint8_t >(data16(r, c) / 256);
+            data8(r, c) = clamp(data16(r, c) / 255, 0, UINT8_MAX);
         }
     }
     return data8;
 }
 
-auto greenReconstruction(size_t r, size_t c, Mat<array<uint8_t, 3>> &data, size_t index) {
+void greenReconstruction(size_t r, size_t c, Mat<array<uint8_t, 3>> &data, size_t index) {
 
-//    if (r < 2 or c < 2 or r >= data.rows() - 2 or c >= data.cols() - 2) {
-//        return 0;
-//    }
 
-    auto tmp1 = protected_data(r, c, data, index) * 2 - protected_data(r, c - 2, data, index) -
-                protected_data(r, c + 2, data, index);
-    auto tmp2 = protected_data(r, c, data, index) * 2 - protected_data(r - 2, c, data, index) -
-                protected_data(r + 2, c, data, index);
+    auto tmp1 = data(r, c)[index] * 2 - data(r, c - 2)[index] - data(r, c + 2)[index];
+    auto tmp2 = data(r, c)[index] * 2 - data(r - 2, c)[index] - data(r + 2, c)[index];
 
-    auto deltaH = std::abs(protected_data(r, c - 1, data, 1) - protected_data(r, c + 1, data, 1)) +
-                  std::abs(tmp1);
-    auto deltaV = std::abs(protected_data(r - 1, c, data, 1) - protected_data(r + 1, c, data, 1)) +
-                  std::abs(tmp2);
+    auto deltaH = std::abs(data(r, c - 1)[GREEN] - data(r, c + 1)[GREEN]) + std::abs(tmp1);
+    auto deltaV = std::abs(data(r - 1, c)[GREEN] - data(r + 1, c)[GREEN]) + std::abs(tmp1) + std::abs(tmp2);
 
     if (deltaH < deltaV) {
-        return (protected_data(r, c - 1, data, 1) + protected_data(r, c + 1, data, 1)) / 2 + tmp1 / 4;
+        data(r, c)[GREEN] = clamp((data(r, c - 1)[GREEN] + data(r, c + 1)[GREEN]) / 2 + (tmp1) / 4, 0, UINT8_MAX);
     } else if (deltaH > deltaV) {
-        return (protected_data(r - 1, c, data, 1) + protected_data(r + 1, c, data, 1)) / 2 + tmp2 / 4;
+        data(r, c)[GREEN] = clamp((data(r - 1, c)[GREEN] + data(r - 1, c)[GREEN]) / 2 + (tmp2) / 4, 0, UINT8_MAX);
     } else {
-        return (protected_data(r - 1, c, data, 1) + protected_data(r + 1, c, data, 1) +
-                protected_data(r, c - 1, data, 1) + protected_data(r, c + 1, data, 1)) / 4 +
-               (4 * protected_data(r, c, data, index) - protected_data(r, c - 2, data, index) -
-                protected_data(r, c + 2, data, index) -
-                protected_data(r - 2, c, data, index) -
-                protected_data(r + 2, c, data, index)) / 8;
+        data(r, c)[GREEN] = clamp(
+                (data(r, c - 1)[GREEN] + data(r, c + 1)[GREEN] + data(r - 1, c)[GREEN] + data(r - 1, c)[GREEN]) / 4 +
+                (tmp1 + tmp2) / 8, 0, UINT8_MAX);
     }
 
 }
 
 void avarages_vert(size_t r, size_t c, Mat<array<uint8_t, 3>> &data, size_t index) {
-    data(r, c)[index] = (protected_data(r - 1, c, data, index) + protected_data(r + 1, c, data, index)) / 2;
-
+    data(r, c)[index] = (data(r - 1, c)[index] + data(r + 1, c)[index]) / 2;
 }
 
 void avarages_horiz(size_t r, size_t c, Mat<array<uint8_t, 3>> &data, size_t index) {
-    data(r, c)[index] = (protected_data(r, c - 1, data, index) + protected_data(r, c - 1, data, index)) / 2;
+    data(r, c)[index] = (data(r, c - 1)[index] + data(r, c + 1)[index]) / 2;
 }
 
 void redBluereconstruction(size_t r, size_t c, Mat<array<uint8_t, 3>> &data, size_t index) {
-//    int index = (index == RED) ? BLUE : RED;
-    auto tmp1 = protected_data(r, c, data, GREEN) * 2 - protected_data(r - 1, c - 1, data, GREEN) -
-                protected_data(r + 1, c + 1, data, GREEN);
-    auto tmp2 = protected_data(r, c, data, GREEN) * 2 - protected_data(r - 1, c + 1, data, GREEN) -
-                protected_data(r + 1, c - 1, data, GREEN);
+
+    auto tmp1 = data(r, c)[GREEN] * 2 - data(r - 1, c - 1)[GREEN] - data(r + 1, c + 1)[GREEN];
+    auto tmp2 = data(r, c)[GREEN] * 2 - data(r - 1, c + 1)[GREEN] - data(r + 1, c - 1)[GREEN];
 
 
-    auto deltaN = abs(protected_data(r - 1, c - 1, data, index) - protected_data(r + 1, c + 1, data, index)) +
-                  abs(tmp1);
-    auto deltaP = abs(protected_data(r - 1, c + 1, data, index) - protected_data(r + 1, c - 1, data, index)) +
-                  abs(tmp2);
+    auto deltaN = abs(data(r - 1, c - 1)[index] - data(r + 1, c + 1)[index]) + abs(tmp1);
+    auto deltaP = abs(data(r - 1, c + 1)[index] - data(r + 1, c - 1)[index]) + abs(tmp2);
 
 
     if (deltaN < deltaP) {
-        data(r, c)[index] =
-                (protected_data(r - 1, c - 1, data, index) + protected_data(r + 1, c + 1, data, index)) / 2 +
-                tmp1 / 4;
+        data(r, c)[index] = clamp((data(r - 1, c - 1)[index] + data(r + 1, c + 1)[index]) / 2 + (tmp1) / 4, 0,
+                                  UINT8_MAX);
     } else if (deltaN > deltaP) {
-        data(r, c)[index] =
-                (protected_data(r - 1, c + 1, data, index) + protected_data(r + 1, c - 1, data, index)) / 2 +
-                tmp2 / 4;
+        data(r, c)[index] = clamp((data(r - 1, c + 1)[index] + data(r + 1, c - 1)[index]) / 2 + (tmp2) / 4, 0,
+                                  UINT8_MAX);
     } else {
-        data(r, c)[index] =
-                (protected_data(r - 1, c - 1, data, index) + protected_data(r + 1, c + 1, data, index) +
-                 protected_data(r - 1, c + 1, data, index) + protected_data(r + 1, c - 1, data, index)) / 4 +
-                (tmp1 + tmp2) / 8;
-
+        data(r, c)[index] = clamp((data(r - 1, c - 1)[index] + data(r + 1, c + 1)[index] +
+                                   data(r - 1, c + 1)[index] + data(r + 1, c - 1)[index]) / 4 +
+                                  (tmp1 + tmp2) / 8, 0, UINT8_MAX);
     }
 }
 
 int main(int argc, char **argv) {
+    auto start = steady_clock::now();
+
     if (argc != 3) {
         perror("Error: just 2 parameters");
         return EXIT_FAILURE;
     }
+
     Mat pgmImage = PGMHelper::parsePGM(argv[1]);
 
     Mat<uint8_t> data8 = Mat<uint8_t>(pgmImage.rows(), pgmImage.cols());
+
     quantize(pgmImage, data8);
 
-    PGMHelper::dumpPGM(data8, argv[2]);
-
+    PGMHelper::dumpPGM(data8, argv[2], "_gray");
 
     Mat<array<uint8_t, 3>> bayer_pattern(data8.rows(), data8.cols());
-
+    unique_ptr<array<uint8_t, 3>> zero = make_unique<array<uint8_t, 3>>(array<uint8_t, 3>{0, 0, 0});
+    bayer_pattern.setZero(zero.get());
     for (int r = 0; r < bayer_pattern.rows(); ++r) {
         for (int c = 0; c < bayer_pattern.cols(); ++c) {
             if (r % 2 == 0) {
@@ -228,33 +214,31 @@ int main(int argc, char **argv) {
                 } else {
                     bayer_pattern(r, c)[1] = data8(r, c);
                 }
-
             } else {
                 if (c % 2 == 0) {
                     bayer_pattern(r, c)[1] = data8(r, c);
                 } else {
                     bayer_pattern(r, c)[2] = data8(r, c);
                 }
-
             }
         }
     }
+    PGMHelper::dumpPPM(bayer_pattern, argv[2], "_bayer");
 
     for (int r = 0; r < bayer_pattern.rows(); ++r) {
         for (int c = 0; c < bayer_pattern.cols(); ++c) {
             if (r % 2 == 0) {
                 if (c % 2 == 0) {
-                    bayer_pattern(r, c)[1] = greenReconstruction(r, c, bayer_pattern, RED);
+                    greenReconstruction(r, c, bayer_pattern, RED);
                 }
             } else {
                 if (c % 2 != 0) {
-                    bayer_pattern(r, c)[1] = greenReconstruction(r, c, bayer_pattern, BLUE);
+                    greenReconstruction(r, c, bayer_pattern, BLUE);
                 }
             }
         }
     }
-
-    PGMHelper::dumpPPM(bayer_pattern, "/home/nicola/Desktop/data_processing/bayer_decode/small_green");
+    PGMHelper::dumpPPM(bayer_pattern, argv[2], "_green");
 
     for (int r = 0; r < bayer_pattern.rows(); ++r) {
         for (int c = 0; c < bayer_pattern.cols(); ++c) {
@@ -275,10 +259,12 @@ int main(int argc, char **argv) {
             }
         }
     }
-    auto tmp = (protected_data(10, 49, bayer_pattern, GREEN));
+    PGMHelper::dumpPPM(bayer_pattern, argv[2], "_interp");
 
-    PGMHelper::dumpPPM(bayer_pattern, "/home/nicola/Desktop/data_processing/bayer_decode/small_color");
 
+    auto stop = steady_clock::now();
+    duration<double, std::milli> elapsed_ms = stop - start;
+    cout << "Elapsed time to execute decompression was :" << elapsed_ms.count() << "ms" << endl;
 
     return EXIT_SUCCESS;
 }
